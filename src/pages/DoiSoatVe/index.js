18f6f972888cf93df296b545ed270c2b7088bc60
dbl173 <dbl17032001@gmail.com>
2023-04-13 10:53:22 +0700

    update

--- src/pages/DoiSoatVe/index.js
+++ src/pages/DoiSoatVe/index.js
@@ -1,5 +1,24 @@
+import classNames from 'classnames/bind';
+import React from 'react';
+import styles from './doisoatve.scss';
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles);
+
 function DoiSoatVe() {
-    return <h2>Đối soát vé</h2>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('header')}>
+                <span>Đối Soát Vé</span>
+            </div>
+            <div className="search">
+                <input placeholder="Tìm bằng số vé" spellCheck={false}></input>
+                <button className={cx('search-btn')}>
+                    <img src={images.search} alt="search" />
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default DoiSoatVe;
