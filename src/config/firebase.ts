b9af29b2f74e1f0d2425b42db779869262fe9a90
dbl173 <dbl17032001@gmail.com>
2023-05-24 21:36:27 +0700

    finish

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -10,13 +10,5 @@ const firebaseConfig = {
     measurementId: 'G-FXDZ98T7X5',
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC3a_V00unh_3gTcgxQdUmmHtTQILvPvhs",
-//   authDomain: "cms-ticket-b5cb4.firebaseapp.com",
-//   projectId: "cms-ticket-b5cb4",
-//   storageBucket: "cms-ticket-b5cb4.appspot.com",
-//   messagingSenderId: "501593339267",
-//   appId: "1:501593339267:web:ce20ca7672aa21afc7b2b6"
-// };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
--- src/views/Home/components/Header/Header.tsx
+++ src/views/Home/components/Header/Header.tsx
@@ -1,36 +1,41 @@
-import { Avatar, Col, Input, Row, Space } from "antd";
-import { AiOutlineMail } from "react-icons/ai";
-import { BiBell } from "react-icons/bi";
-import React from "react";
-import { BiSearch } from "react-icons/bi";
+import { Avatar, Col, Input, Row, Space } from 'antd';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BiBell } from 'react-icons/bi';
+import React from 'react';
+import { BiSearch } from 'react-icons/bi';
 const Header: React.FC = () => {
-  return (
-    <Row className="header-container">
-      <Col span={12}>
-        <Row align="middle" style={{ height: "100%" }}>
-          <Input suffix={<BiSearch size={20} />} placeholder="Search" />
+    return (
+        <Row className="header-container">
+            <Col span={12}>
+                <Row align="middle" style={{ height: '100%' }}>
+                    <Input suffix={<BiSearch size={20} />} placeholder="Search" />
+                </Row>
+            </Col>
+            <Col span={12}>
+                <Row justify="end" align="middle" style={{ paddingRight: '32px' }}>
+                    <Space
+                        size={27}
+                        style={{
+                            height: '85px',
+                        }}
+                    >
+                        <Row align="middle">
+                            <AiOutlineMail size={20} />
+                        </Row>
+                        <Row align="middle">
+                            <BiBell size={20} />
+                        </Row>
+                        <Avatar size={48}>
+                            <img
+                                src="https://files.fullstack.edu.vn/f8-prod/user_avatars/1/623d4b2d95cec.png"
+                                alt="account"
+                            />
+                        </Avatar>
+                    </Space>
+                </Row>
+            </Col>
         </Row>
-      </Col>
-      <Col span={12}>
-        <Row justify="end" align="middle" style={{ paddingRight: "32px" }}>
-          <Space
-            size={27}
-            style={{
-              height: "85px",
-            }}
-          >
-            <Row align="middle">
-              <AiOutlineMail size={20} />
-            </Row>
-            <Row align="middle">
-              <BiBell size={20} />
-            </Row>
-            <Avatar size={48}>BaLoi</Avatar>
-          </Space>
-        </Row>
-      </Col>
-    </Row>
-  );
+    );
 };
 
 export default Header;
